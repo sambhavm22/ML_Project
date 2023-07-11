9e11c32aac10cfa61b320d172a7b250150931009
sambhavm22 <58766591+sambhavm22@users.noreply.github.com>
2023-07-11 23:18:57 +0200

    model trainer with hyperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,10 @@ from dataclasses import dataclass
 from src.components.data_transformtion import DataTransformation
 from src.components.data_transformtion import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
+
 
 @dataclass
 class DataIngestionConfig:
@@ -54,5 +58,8 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
     
